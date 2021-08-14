8bdd3f775b68010db36e04b736cfeae60cbc65e2
daniel zohar <daniel zohar@herolo.co.il>
2021-08-14 23:40:54 +0300

    update(converter): finish

--- src/app/currency-dashboard/currency-converter/currency-converter.component.ts
+++ src/app/currency-dashboard/currency-converter/currency-converter.component.ts
@@ -44,15 +44,13 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit {
     this.formGroup.valueChanges.pipe(
       debounceTime(800),
       distinctUntilChanged(),
-      switchMap((formValue: Record<string, any>) => {
-        return this.getConversionRateResults(formValue.amount);
-      }),
+      switchMap((formValue: Record<string, any>) => this.getConversionRateResults(formValue.amount)),
       untilDestroyed(this)
     ).subscribe();
   }
 
   getConversionRateResults(amount: number): Observable<never> | Observable<ConversionRate> {
-    let resultObs$: Observable<never> | Observable<ConversionRate> = EMPTY.pipe(ignoreElements());
+    let response$: Observable<never> | Observable<ConversionRate> = EMPTY.pipe(ignoreElements());
 
     if (!amount) {
       this.converterService.convertingText = '';
@@ -70,7 +68,7 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit {
           to
         };
 
-        resultObs$ = this.converterService.getConversionRate(this._params).pipe(
+        response$ = this.converterService.getConversionRate(this._params).pipe(
           tap((data: ConversionRate) => {
             if (data?.rates[to]) {
               this.converterService.convertingText = this.prepareConvertingText(amount, data.rates[to], from, to);
@@ -86,7 +84,7 @@ export class CurrencyConverterComponent implements OnInit, AfterViewInit {
       }
     }
 
-    return resultObs$;
+    return response$;
   }
 
   prepareConvertingText(amountFrom: number, amountTo: number, fromCode: string, toCode: string): string {
